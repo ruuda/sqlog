50fb8cccd6b257bc776223e687332f8b11ae35fd
Ruud van Asseldonk <dev@veniogames.com>
2019-02-04 22:35:01 +0100

    Add log line parser
    Ugly, but then again, the default Nginx log format is extremely ugly.

--- sqlog.py
+++ sqlog.py
@@ -11,6 +11,65 @@ Usage:
 
 import sys
 import sqlite3
+import typing
+
+from typing import NamedTuple, Optional
+
+class Row(NamedTuple):
+  remote_addr: str
+  time_local: str
+  method: Optional[str]
+  url: Optional[str]
+  protocol: Optional[str]
+  status: int
+  body_bytes_sent: int
+  referer: Optional[str]
+  user_agent: Optional[str]
+
+
+def parse_line(line: str) -> Row:
+  # Parse the basic line.
+  remote_addr, line = line.split(' - ', 1)
+  remote_user, line = line.split(' [', 1)
+  time_local_ugly, line = line.split('] ', 1)
+  assert line[:1] == '"'
+  # TODO: Can the request include escaped quotes?
+  request, line = line[1:].split('" ', 1)
+  status_str, line = line.split(' ', 1)
+  body_bytes_str, line = line.split(' ', 1)
+  assert line[:1] == '"'
+  # TODO: Can the referer include escaped quotes?
+  referer, line = line[1:].split('" "', 1)
+  # TODO: Can the user agent include escaped quotes?
+  user_agent, tail = line.split('"', 1)
+  assert tail == '\n'
+
+  # Split request in method, url, and protocol.
+  # Sometimes the request contains utter garbage.
+  method, url, protocol = None, None, None
+  if request.count(' ') == 2:
+    method, request = request.split(' ', 1)
+    url, protocol = request.rsplit(' ', 1)
+
+  # Nginx logs unknown things as a dash.
+  referer = None if referer == '-' else referer
+  user_agent = None if user_agent == '-' else user_agent
+
+  # TODO: Parse time.
+  time_local_iso8601 = time_local_ugly
+
+  return Row(
+    remote_addr,
+    time_local_iso8601,
+    method,
+    url,
+    protocol,
+    int(status_str),
+    int(body_bytes_str),
+    referer,
+    user_agent,
+  )
+
 
 def create_table(conn: sqlite3.Connection) -> None:
   """
@@ -21,13 +80,13 @@ def create_table(conn: sqlite3.Connection) -> None:
     (
       remote_addr     text not null,
       time_local      text not null,
-      method          text not null,
-      url             text not null,
-      protocol        text not null,
+      method          text null,
+      url             text null,
+      protocol        text null,
       status          int not null,
       body_bytes_sent int not null,
       referer         text null,
-      user_agent      text not null,
+      user_agent      text null,
       constraint unique_visit unique (remote_addr, time_local, method, url)
     );
   """)
@@ -39,6 +98,10 @@ def create_table(conn: sqlite3.Connection) -> None:
 def main(fname: str) -> None:
   with sqlite3.connect(fname) as conn:
     create_table(conn)
+
+    for line in sys.stdin:
+      row = parse_line(line)
+      print(row)
 
 
 if __name__ == '__main__':
